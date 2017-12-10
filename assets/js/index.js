ccfa4eca7cb7142a47aa49a0a86f0bb89f1d33f0
xiaolin <462441355@qq.com>
2017-12-10 14:05:43 +0800

    自动化构建 browerify 加载第三方类库

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+$ = require('jquery')
 $(function(){
     console.log(require('./ceshi'))
     // alert(1)
--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,16 @@ fs = require('fs'),
 sequence = require('run-sequence')
 
 gulp.task('default', function () {
-    sequence('mainjs')
+    sequence('vendorjs','mainjs')
 })
 
 gulp.task('mainjs', function () {
-    var b = browserify({entries: ['assets/js/index.js'], cache: {}, packageCache: {}, plugin: [watchify]});
+    var b = browserify({
+        entries: ['assets/js/index.js'],
+        cache: {},
+        packageCache: {},
+        plugin: [watchify]
+    }).external('jquery');
 
     var bundle = function () {
         b.bundle()
@@ -20,6 +25,14 @@ gulp.task('mainjs', function () {
     bundle();
 
     b.on('update', bundle)
+})
+
+gulp.task('vendorjs',function(){
+    var b = browserify().require('./bower_components/jquery/dist/jquery.js',{
+        expose: 'jquery'
+    })
+    .bundle()
+    .pipe(fs.createWriteStream('www/js/vendor.js'))
 })
 
 // gulp.task('watch', function () {     gulp         .watch(['assets/js/*'],
